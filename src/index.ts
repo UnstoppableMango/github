bb5ccbc405e03fe8979d42c2529310a06394de4e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-27 23:19:52 -0500

    Format

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import * as gh from '@pulumi/github';
+import * as gh from "@pulumi/github";
 
-const tlsRepo = new gh.Repository('tls', {
-	allowAutoMerge: true,
-	allowMergeCommit: false,
-	allowRebaseMerge: false,
-	allowSquashMerge: true,
-	description: 'My TLS infrastructure',
-	deleteBranchOnMerge: true,
-	hasDiscussions: false,
-	hasIssues: false,
-	hasProjects: false,
-	hasWiki: false,
-	licenseTemplate: 'mit',
-	name: 'tls',
-	squashMergeCommitMessage: 'COMMIT_MESSAGES',
-	squashMergeCommitTitle: 'COMMIT_OR_PR_TITLE',
-	visibility: 'private',
+const tlsRepo = new gh.Repository("tls", {
+  allowAutoMerge: true,
+  allowMergeCommit: false,
+  allowRebaseMerge: false,
+  allowSquashMerge: true,
+  description: "My TLS infrastructure",
+  deleteBranchOnMerge: true,
+  hasDiscussions: false,
+  hasIssues: false,
+  hasProjects: false,
+  hasWiki: false,
+  licenseTemplate: "mit",
+  name: "tls",
+  squashMergeCommitMessage: "COMMIT_MESSAGES",
+  squashMergeCommitTitle: "COMMIT_OR_PR_TITLE",
+  visibility: "private",
 });
