a2391eba54681331cb7c23f6e5ea4c4ebfe4e0f1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-30 00:24:13 -0500

    Updated code formatting and added new repository ruleset
    The code changes include updates to the formatting of various configuration files, correcting a typo in the Makefile, and adding a new repository ruleset in index.ts. The updated formatting includes consistent use of tabs for indentation and other stylistic preferences. In index.ts, a new GitHub RepositoryRuleset has been introduced with specific conditions and rules.

--- index.ts
+++ index.ts
@@ -1,21 +1,50 @@
-import * as gh from "@pulumi/github";
+import * as gh from '@pulumi/github';
 
-const tlsRepo = new gh.Repository("tls", {
+const tlsRepo = new gh.Repository('tls', {
 	allowAutoMerge: true,
 	allowMergeCommit: false,
 	allowRebaseMerge: false,
 	allowSquashMerge: true,
-	description: "My TLS infrastructure",
+	description: 'My TLS infrastructure',
 	deleteBranchOnMerge: true,
 	hasDiscussions: false,
 	hasIssues: true,
 	hasProjects: false,
 	hasWiki: false,
-	licenseTemplate: "mit",
-	name: "tls",
-	squashMergeCommitMessage: "COMMIT_MESSAGES",
-	squashMergeCommitTitle: "COMMIT_OR_PR_TITLE",
-	visibility: "private",
+	licenseTemplate: 'mit',
+	name: 'tls',
+	squashMergeCommitMessage: 'COMMIT_MESSAGES',
+	squashMergeCommitTitle: 'COMMIT_OR_PR_TITLE',
+	visibility: 'private',
 }, {
 	protect: true,
-});
+},);
+
+const tlsRuleset = new gh.RepositoryRuleset('tls', {
+	name: 'main',
+	repository: tlsRepo.name,
+	enforcement: 'active',
+	target: 'branch',
+	conditions: {
+		refName: {
+			includes: ['~DEFAULT_BRANCH',],
+			excludes: [],
+		},
+	},
+	rules: {
+		deletion: true,
+		pullRequest: {
+			dismissStaleReviewsOnPush: true,
+			requiredReviewThreadResolution: true,
+			requireLastPushApproval: true,
+		},
+		nonFastForward: true,
+		requiredLinearHistory: true,
+		requiredSignatures: true,
+		requiredStatusChecks: {
+			requiredChecks: [{
+				context: 'Main',
+			},],
+		},
+	},
+},);
