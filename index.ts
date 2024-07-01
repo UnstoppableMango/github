51f515977bd037dd82c5da35bc521302c68e545f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-01 16:34:03 +0000

    Add the Iowa Dems mailer repo (#11)
    * Refactor into function
    
    * Add the iowa dems mailer repo

--- index.ts
+++ index.ts
@@ -1,21 +1,35 @@
 import * as gh from '@pulumi/github';
+import { CustomResourceOptions } from '@pulumi/pulumi';
 
-const tlsRepo = new gh.Repository('tls', {
-	allowAutoMerge: true,
-	allowMergeCommit: false,
-	allowRebaseMerge: false,
-	allowSquashMerge: true,
-	description: 'My TLS infrastructure',
-	deleteBranchOnMerge: true,
-	hasDiscussions: false,
-	hasIssues: true,
-	hasProjects: false,
-	hasWiki: false,
-	licenseTemplate: 'mit',
-	name: 'tls',
-	squashMergeCommitMessage: 'COMMIT_MESSAGES',
-	squashMergeCommitTitle: 'COMMIT_OR_PR_TITLE',
-	visibility: 'private',
-}, {
+function privateRepo(
+	name: string,
+	description?: string,
+	opts?: CustomResourceOptions,
+): gh.Repository {
+	return new gh.Repository(name, {
+		name,
+		description,
+		allowAutoMerge: true,
+		allowMergeCommit: false,
+		allowRebaseMerge: false,
+		allowSquashMerge: true,
+		deleteBranchOnMerge: true,
+		hasDiscussions: false,
+		hasIssues: true,
+		hasProjects: false,
+		hasWiki: false,
+		licenseTemplate: 'mit',
+		squashMergeCommitMessage: 'COMMIT_MESSAGES',
+		squashMergeCommitTitle: 'COMMIT_OR_PR_TITLE',
+		visibility: 'private',
+	}, opts);
+}
+
+const tlsRepo = privateRepo('tls', 'My TLS infrastructure', {
 	protect: true,
 });
+
+const iowaDems = privateRepo(
+	'iowa-dems-mailer',
+	'Iowa Democrats mailing application',
+);
