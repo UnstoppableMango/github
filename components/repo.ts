350229510277fb7ec36953c509f7bf29738abce9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-05 14:10:55 -0500

    Added GitHub repository management classes
    Introduced abstract Repo class and its subclasses PrivateRepo and PublicRepo for managing GitHub repositories. These classes allow setting various repository properties like visibility, merge options, deletion policy etc. Also added support for creating RepositoryRuleset in the PublicRepo class.

---
+++ components/repo.ts
@@ -0,0 +1,110 @@
+import { ComponentResource, ComponentResourceOptions, Input } from '@pulumi/pulumi';
+import * as gh from '@pulumi/github';
+
+interface RepoArgs {
+	overrides: Partial<gh.RepositoryArgs>;
+}
+
+abstract class Repo extends ComponentResource {
+	public readonly repo: gh.Repository;
+
+	constructor(type: string, name: string, args: RepoArgs, opts?: ComponentResourceOptions) {
+		super(type, name, args, opts);
+		if (opts?.urn) return; // Refreshing
+
+		const repo = new gh.Repository(name, {
+			allowAutoMerge: true,
+			allowMergeCommit: false,
+			allowRebaseMerge: false,
+			allowSquashMerge: true,
+			deleteBranchOnMerge: true,
+			hasDiscussions: false,
+			hasIssues: true,
+			hasProjects: false,
+			hasWiki: false,
+			licenseTemplate: 'mit',
+			squashMergeCommitMessage: 'COMMIT_MESSAGES',
+			squashMergeCommitTitle: 'COMMIT_OR_PR_TITLE',
+			...args.overrides,
+		}, opts);
+
+		this.repo = repo;
+	}
+}
+
+export interface PrivateRepoArgs {
+	description: Input<string>;
+}
+
+export class PrivateRepo extends Repo {
+	constructor(name: string, args: PrivateRepoArgs, opts?: ComponentResourceOptions) {
+		super('unmango:github:PrivateRepo', name, {
+			overrides: {
+				name,
+				description: args.description,
+				visibility: 'private',
+			},
+		}, opts);
+
+		const repo = this.repo;
+
+		this.registerOutputs({ repo });
+	}
+}
+
+
+export interface PublicRepoArgs {
+	description: Input<string>;
+}
+
+export class PublicRepo extends Repo {
+	public readonly mainRuleset: gh.RepositoryRuleset;
+
+	constructor(name: string, args: PublicRepoArgs, opts?: ComponentResourceOptions) {
+		super('unmango:github:PrivateRepo', name, {
+			overrides: {
+				name,
+				description: args.description,
+				visibility: 'public',
+			},
+		}, opts);
+
+		const repo = this.repo;
+
+		const mainRuleset = new gh.RepositoryRuleset(name, {
+			name: 'main',
+			repository: repo.name,
+			enforcement: 'active',
+			target: 'branch',
+			conditions: {
+				refName: {
+					includes: ['~DEFAULT_BRANCH'],
+					excludes: [],
+				},
+			},
+			rules: {
+				deletion: true,
+				pullRequest: {
+					dismissStaleReviewsOnPush: true,
+					requiredReviewThreadResolution: true,
+					requireLastPushApproval: true,
+				},
+				nonFastForward: true,
+				requiredLinearHistory: true,
+				requiredSignatures: true,
+				requiredStatusChecks: {
+					requiredChecks: [{
+						context: 'Main',
+					}],
+				},
+			},
+		});
+
+		this.mainRuleset = mainRuleset;
+
+		this.registerOutputs({
+			repo,
+			mainRuleset,
+		});
+	}
+}
