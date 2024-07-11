e4252da677d64045800a816f2ae896e66218cb4c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-11 18:41:49 +0000

    Make status checks optional (#26)
    * Make status checks optional
    
    * Format

--- components/publicRepo.ts
+++ components/publicRepo.ts
@@ -1,54 +1,65 @@
 import * as gh from '@pulumi/github';
+import { RepositoryRulesetRules, RepositoryRulesetRulesRequiredStatusChecks } from '@pulumi/github/types/input';
 import { ComponentResourceOptions, Input } from '@pulumi/pulumi';
 import { Repo } from './repo';
 
 export interface PublicRepoArgs {
 	description: Input<string>;
+	requiredChecks?: RepositoryRulesetRulesRequiredStatusChecks['requiredChecks'];
 }
 
 export class PublicRepo extends Repo {
 	public readonly mainRuleset: gh.RepositoryRuleset;
 
-	constructor(name: string, args: PublicRepoArgs, opts?: ComponentResourceOptions) {
-		super('unmango:github:PublicRepo', name, {
-			overrides: {
-				name,
-				description: args.description,
-				visibility: 'public',
-				allowAutoMerge: true,
+	constructor(
+		name: string,
+		args: PublicRepoArgs,
+		opts?: ComponentResourceOptions,
+	) {
+		super(
+			'unmango:github:PublicRepo',
+			name,
+			{
+				overrides: {
+					name,
+					description: args.description,
+					visibility: 'public',
+					allowAutoMerge: true,
+				},
 			},
-		}, opts);
+			opts,
+		);
 
 		const repo = this.repo;
 
-		const mainRuleset = new gh.RepositoryRuleset(name, {
-			name: 'main',
-			repository: repo.name,
-			enforcement: 'active',
-			target: 'branch',
-			conditions: {
-				refName: {
-					includes: ['~DEFAULT_BRANCH'],
-					excludes: [],
+		const mainRuleset = new gh.RepositoryRuleset(
+			name,
+			{
+				name: 'main',
+				repository: repo.name,
+				enforcement: 'active',
+				target: 'branch',
+				conditions: {
+					refName: {
+						includes: ['~DEFAULT_BRANCH'],
+						excludes: [],
+					},
+				},
+				rules: {
+					deletion: true,
+					pullRequest: {
+						dismissStaleReviewsOnPush: true,
+						requiredReviewThreadResolution: true,
+						requireLastPushApproval: true,
+					},
+					nonFastForward: true,
+					requiredLinearHistory: true,
+					requiredSignatures: true,
+					requiredStatusChecks: getRequiredStatusChecks(args.requiredChecks),
 				},
 			},
-			rules: {
-				deletion: true,
-				pullRequest: {
-					dismissStaleReviewsOnPush: true,
-					requiredReviewThreadResolution: true,
-					requireLastPushApproval: true,
-				},
-				nonFastForward: true,
-				requiredLinearHistory: true,
-				requiredSignatures: true,
-				requiredStatusChecks: {
-					requiredChecks: [{
-						context: 'Main',
-					}],
-				},
-			},
-		}, { parent: this });
+			{ parent: this },
+		);
 
 		this.mainRuleset = mainRuleset;
 
@@ -57,4 +68,11 @@ export class PublicRepo extends Repo {
 			mainRuleset,
 		});
 	}
+}
+
+function getRequiredStatusChecks(
+	checks: PublicRepoArgs['requiredChecks'],
+): RepositoryRulesetRules['requiredStatusChecks'] {
+	if (!checks) return;
+	return { requiredChecks: checks };
 }
