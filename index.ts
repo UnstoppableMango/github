8ceff9555cc7fd53752665601c773c224c92c4b8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-19 13:01:14 -0500

    Import a bunch of my other repos (#161)
    * Import advent-of-code
    
    * Import xml
    
    * Import rest
    
    * Import slacker-bot
    
    * Import http
    
    * Import audio
    
    * Import the-cluster
    
    * Import infra
    
    * Import unstoppablemango.io
    
    * Import tdl
    
    * Import dotnet-proxmox-client
    
    * Import mango-mtg
    
    * Import proxmox-client
    
    * Import xmage-docker
    
    * Import pulumi-proxmox
    
    * Import imaug
    
    * Import pulumi-imaug
    
    * Import multi-downloader-nx-docker
    
    * Import CliWrap.FSharp
    
    * Import Docker.Dotnet.FSharp
    
    * Import Docker.FSharp
    
    * Import pulumi-kubernetes-the-hard-way
    
    * Import pulumi-ci-mgmt
    
    * Import dotfiles
    
    * Import fsharp-property-testing
    
    * Import palumi-world
    
    * Import blocky-controller
    
    * Also that stuff

--- index.ts
+++ index.ts
@@ -5,9 +5,199 @@ const integrationIds = {
 	github: 15368,
 };
 
-const pki = new PrivateRepo('pki', {
-	description: 'My private key infrastructure',
+const adventOfCode = new gh.Repository('advent-of-code', {
+	name: 'advent-of-code',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'Advent of Code solutions in various languages',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	topics: ['advent-of-code'],
+	visibility: 'public',
 }, { protect: true });
+
+const audio = new gh.Repository('audio', {
+	name: 'audio',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowRebaseMerge: false,
+	deleteBranchOnMerge: true,
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const blockyController = new gh.Repository('blocky-controller', {
+	name: 'blocky-controller',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'Blocky kubernetes controller sandbox',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const cliwrapFsharp = new gh.Repository('CliWrap.FSharp', {
+	name: 'CliWrap.FSharp',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Idiomatic F# support for CliWrap',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const dockerDotnetFsharp = new gh.Repository('Docker.Dotnet.FSharp', {
+	name: 'Docker.Dotnet.FSharp',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Idiomatic F# support for Docker.DotNet',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const dockerFsharp = new gh.Repository('Docker.FSharp', {
+	name: 'Docker.FSharp',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Docker computation expression and client written in F#',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const dotfiles = new gh.Repository('dotfiles', {
+	name: 'dotfiles',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	hasDownloads: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
+const dotnetProxmoxClient = new gh.Repository('dotnet-proxmox-client', {
+	name: 'dotnet-proxmox-client',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: '.NET client for the Proxmox REST API',
+	hasDownloads: true,
+	hasIssues: true,
+	hasWiki: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
+const everybodyCodes = new PublicRepo('everybody-codes', {
+	description: 'Everybody Codes solutions in various languages',
+});
+
+const fsharpPropertyTesting = new gh.Repository('fsharp-property-testing', {
+	name: 'fsharp-property-testing',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Lightning talk for property testing in F#',
+	hasDownloads: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const gast = new PublicRepo('gast', {
+	description: 'ASTs for everyone',
+	topics: ['ast', 'codegen', 'protobuf', 'grpc', 'buf'],
+	requiredChecks: [
+		{ context: 'Build and Test', integrationId: integrationIds.github },
+	],
+});
 
 const hosts = new PublicRepo('hosts', {
 	description: 'My on-prem server infrastructure',
@@ -24,24 +214,57 @@ const hosts = new PublicRepo('hosts', {
 	}),
 });
 
+const http = new gh.Repository('http', {
+	name: 'http',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
+const imaug = new gh.Repository('imaug', {
+	name: 'imaug',
+	description: 'Code used in presentations for the Iowa Microsoft Azure User Group',
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	hasWiki: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const infra = new gh.Repository('infra', {
+	name: 'infra',
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	hasDownloads: true,
+	hasIssues: true,
+	visibility: 'private',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
 const johnstonDems = new PrivateRepo('johnston-dems-mailer', {
 	description: 'Johnston Democrats mailing application',
-});
-
-const pulumiBun = new PublicRepo('pulumi-bun', {
-	description: 'Experimental Pulumi support for Bun',
-});
-
-const everybodyCodes = new PublicRepo('everybody-codes', {
-	description: 'Everybody Codes solutions in various languages',
-});
-
-const gast = new PublicRepo('gast', {
-	description: 'ASTs for everyone',
-	topics: ['ast', 'codegen', 'protobuf', 'grpc', 'buf'],
-	requiredChecks: [
-		{ context: 'Build and Test', integrationId: integrationIds.github },
-	],
 });
 
 const lang = new PublicRepo('lang', {
@@ -51,6 +274,22 @@ const lang = new PublicRepo('lang', {
 		integrationId: integrationIds.github,
 	}],
 });
+
+const mangoMtg = new gh.Repository('mango-mtg', {
+	name: 'mango-mtg',
+	description: 'Digital Magic: The Gathering',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
 
 const minecraftOperator = new gh.Repository('minecraft-operator', {
 	description: 'An operator for managing minecraft servers on Kubernetes',
@@ -71,6 +310,30 @@ const minecraftManager = new PublicRepo('minecraft-manager', {
 	],
 });
 
+const multiDownloaderNxDocker = new gh.Repository('multi-downloader-nx-docker', {
+	name: 'multi-downloader-nx-docker',
+	description: 'Docker image for anidl/multi-downloader-nx',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	topics: [
+		'anime',
+		'crunchyroll',
+		'docker',
+		'downloader',
+		'funimation',
+		'utility',
+	],
+	visibility: 'public',
+}, { protect: true });
+
 const nixos = new PublicRepo('nixos', {
 	description: 'My NixOS source',
 	requiredChecks: [{
@@ -82,6 +345,24 @@ const nixos = new PublicRepo('nixos', {
 const ouranosis = new PublicRepo('ouranosis', {
 	description: 'A game-ish kinda thing',
 });
+
+const palumiWorld = new gh.Repository('palumi-world', {
+	name: 'palumi-world',
+	description: 'My Palworld install',
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	hasWiki: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
 
 const perryOperator = new PublicRepo('perry-operator', {
 	description: `A platypus? Perry the platypus?!?`,
@@ -107,6 +388,121 @@ const piaManualConnections = new PublicRepo('pia-manual-connections', {
 	],
 });
 
+const pki = new PrivateRepo('pki', {
+	description: 'My private key infrastructure',
+}, { protect: true });
+
+const proxmoxClient = new gh.Repository('proxmox-client', {
+	name: 'proxmox-client',
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'Proxmox client libraries generated via OpenAPI Generator',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const pulumiBun = new PublicRepo('pulumi-bun', {
+	description: 'Experimental Pulumi support for Bun',
+});
+
+const pulumiCiMgmt = new gh.Repository('pulumi-ci-mgmt', {
+	name: 'pulumi-ci-mgmt',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'CI automation for Pulumi providers based on pulumi/ci-mgmt',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+const pulumiImaug = new gh.Repository('pulumi-imaug', {
+	name: 'pulumi-imaug',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'A Pulumi component provider for the Iowa Microsoft Azure User Group presentation',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	template: {
+		owner: 'pulumi',
+		repository: 'pulumi-component-provider-ts-boilerplate',
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const pulumiKubernetesTheHardWay = new gh.Repository('pulumi-kubernetes-the-hard-way', {
+	name: 'pulumi-kubernetes-the-hard-way',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'A Pulumi component provider that implements Kelsey Hightower\'s Kubernetes the Hard Way',
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	template: {
+		owner: 'pulumi',
+		repository: 'pulumi-component-provider-go-boilerplate',
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const pulumiProxmox = new gh.Repository('pulumi-proxmox', {
+	name: 'pulumi-proxmox',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	template: {
+		owner: 'pulumi',
+		repository: 'pulumi-component-provider-ts-boilerplate',
+	},
+	visibility: 'public',
+}, { protect: true });
+
 const renovateConfig = new PublicRepo('renovate-config', {
 	description: `UnstoppableMango's Renovate presets`,
 	topics: ['renovate', 'cicd', 'bun'],
@@ -114,6 +510,112 @@ const renovateConfig = new PublicRepo('renovate-config', {
 		{ context: 'Validate', integrationId: integrationIds.github },
 	],
 });
+
+const rest = new gh.Repository('rest', {
+	name: 'rest',
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'Just another .NET REST client',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
+const slackerBot = new gh.Repository('slacker-bot', {
+	name: 'slacker-bot',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitMessage: 'PR_BODY',
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
+const tdl = new gh.Repository('tdl', {
+	name: 'tdl',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Type description language and codegen suite',
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'enabled',
+		},
+		secretScanningPushProtection: {
+			status: 'enabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+	webCommitSignoffRequired: true,
+}, { protect: true });
+
+const theCluster = new gh.Repository('the-cluster', {
+	name: 'the-cluster',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowUpdateBranch: true,
+	deleteBranchOnMerge: true,
+	description: 'Source for THECLUSTER',
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+	webCommitSignoffRequired: true,
+}, { protect: true });
+
+const unstoppablemango_io = new gh.Repository('unstoppablemango.io', {
+	name: 'unstoppablemango.io',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	allowRebaseMerge: false,
+	deleteBranchOnMerge: true,
+	description: 'A website about me for random garbage',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
 
 const ux = new PublicRepo('ux', {
 	description: `The universal codegen framework`,
@@ -123,21 +625,82 @@ const ux = new PublicRepo('ux', {
 	],
 });
 
+const xmageDocker = new gh.Repository('xmage-docker', {
+	name: 'xmage-docker',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+}, { protect: true });
+
+const xml = new gh.Repository('xml', {
+	name: 'xml',
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'A .NET XML serializer',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	visibility: 'public',
+	vulnerabilityAlerts: true,
+}, { protect: true });
+
 export const repos = [
+	adventOfCode.name,
+	audio.name,
+	blockyController.name,
+	cliwrapFsharp.name,
+	dockerDotnetFsharp.name,
+	dockerFsharp.name,
+	dotfiles.name,
+	dotnetProxmoxClient.name,
 	everybodyCodes.repo.name,
+	fsharpPropertyTesting.name,
 	gast.repo.name,
 	hosts.repo.name,
+	http.name,
+	imaug.name,
+	infra.name,
 	johnstonDems.repo.name,
 	lang.repo.name,
+	mangoMtg.name,
 	minecraftManager.repo.name,
 	minecraftOperator.name,
+	multiDownloaderNxDocker.name,
 	nixos.repo.name,
 	ouranosis.repo.name,
+	palumiWorld.name,
 	perryOperator.repo.name,
 	pfsenseOperator.repo.name,
 	piaManualConnections.repo.name,
 	pki.repo.name,
+	proxmoxClient.name,
 	pulumiBun.repo.name,
+	pulumiCiMgmt.name,
+	pulumiImaug.name,
+	pulumiKubernetesTheHardWay.name,
+	pulumiProxmox.name,
 	renovateConfig.repo.name,
+	rest.name,
+	slackerBot.name,
+	tdl.name,
+	theCluster.name,
+	unstoppablemango_io.name,
 	ux.repo.name,
+	xmageDocker.name,
+	xml.name,
 ];
