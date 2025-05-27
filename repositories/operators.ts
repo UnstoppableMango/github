bb263b82c499f14a4698dc0d906b36d8aef6b435
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-26 20:31:00 -0500

    Split out everything into files

---
+++ repositories/operators.ts
@@ -0,0 +1,38 @@
+import * as gh from '@pulumi/github';
+import { integrationIds, PublicRepo } from '../components';
+
+export const blockyController = new gh.Repository('blocky-controller', {
+	name: 'blocky-controller',
+	allowAutoMerge: true,
+	allowMergeCommit: false,
+	deleteBranchOnMerge: true,
+	description: 'Blocky kubernetes controller sandbox',
+	hasDownloads: true,
+	hasIssues: true,
+	securityAndAnalysis: {
+		secretScanning: {
+			status: 'disabled',
+		},
+		secretScanningPushProtection: {
+			status: 'disabled',
+		},
+	},
+	squashMergeCommitTitle: 'PR_TITLE',
+	visibility: 'public',
+}, { protect: true });
+
+export const perryOperator = new PublicRepo('perry-operator', {
+	description: `A platypus? Perry the platypus?!?`,
+	topics: ['kubernetes', 'pulumi', 'operator'],
+	requiredChecks: [
+		{ context: 'Build and Test', integrationId: integrationIds.github },
+	],
+});
+
+export const pfsenseOperator = new PublicRepo('pfsense-operator', {
+	description: 'An operator for deploying and managing pfSense on Kubernetes',
+	requiredChecks: [{
+		context: 'Build and Test',
+		integrationId: integrationIds.github,
+	}],
+});
