a36592b706391274203b3328005e27a976801ef6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-04 22:19:52 -0500

    Add pulumi2crd (#177)

--- repositories/ux.ts
+++ repositories/ux.ts
@@ -9,8 +9,19 @@ export const gast = new PublicRepo('gast', {
 	],
 });
 
+// Not sure why I started with this, the goal is to convert package specs
+// to CRDs. I'll generalize it to PCL/OpenAPI if the need arises...
 export const pcl2openapi = new PublicRepo('pcl2openapi', {
 	description: 'Converts Pulumi Configuration Language (PCL) to OpenAPI',
+	requiredChecks: [
+		{ context: 'build', integrationId: integrationIds.github },
+		{ context: 'lint', integrationId: integrationIds.github },
+		{ context: 'docker', integrationId: integrationIds.github },
+	],
+});
+
+export const pulumi2crd = new PublicRepo('pulumi2crd', {
+	description: 'Converts Pulumi package specs to Custom Resource Definitions (CRDs)',
 	requiredChecks: [
 		{ context: 'build', integrationId: integrationIds.github },
 		{ context: 'lint', integrationId: integrationIds.github },
