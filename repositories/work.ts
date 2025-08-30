d650234281fa6493efd9be10a543062326a07d85
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-08-30 13:04:28 -0500

    Add a repo for kubernetes-the-wrong-way

--- repositories/work.ts
+++ repositories/work.ts
@@ -1,5 +1,10 @@
-import { PrivateRepo } from '../components';
+import { PrivateRepo, PublicRepo } from '../components';
 
 export const fabricTesting = new PrivateRepo('fabric-testing', {
 	description: 'Fiddling with MS Fabric',
+});
+
+export const kubernetesTheWrongWay = new PublicRepo('kubernetes-the-wrong-way', {
+	description: 'A CRUD API using Kubernetes APIs',
+	topics: ['kubernetes', 'technically-speaking', 'rest'],
 });
