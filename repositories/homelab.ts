cc1fdcefd07103cc61bcb8dac1614f992cbc7cc6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-09-08 16:44:17 -0500

    Add wireguard-cni and thecluster.io repos

--- repositories/homelab.ts
+++ repositories/homelab.ts
@@ -1,5 +1,12 @@
-import { PrivateRepo } from '../components';
+import { integrationIds, PrivateRepo, PublicRepo } from '../components';
 
-const pfsense = new PrivateRepo('pfsense', {
+export const pfsense = new PrivateRepo('pfsense', {
 	description: 'pfSense configuration',
+});
+
+export const theclusterIo = new PublicRepo('thecluster.io', {
+	description: 'Web portal to THECLUSTER',
+	requiredChecks: [
+		{ context: 'Build and Test', integrationId: integrationIds.github },
+	],
 });
