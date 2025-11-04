14f3aa95afe14790969e2a040a9aec0150994cb5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-04 13:02:51 -0600

    Repository for collecting ux plugins (#261)
    * Repository for collecting ux plugins
    
    * Fmt

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as gh from '@pulumi/github';
 import { integrationIds, PrivateRepo, PublicRepo } from './components';
 import {
+	a2b,
 	audio,
 	blockyController,
 	cliwrapFsharp,
@@ -221,6 +222,7 @@ const unstoppablemango_io = new gh.Repository('unstoppablemango.io', {
 export const repos = [
 	adventOfCode.name,
 	audio.name,
+	a2b.repo.name,
 	blockyController.name,
 	cliwrapFsharp.name,
 	dockerDotnetFsharp.name,
