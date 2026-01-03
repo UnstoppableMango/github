e0c1372fa7ac929a5dfb064dbf4ba7590c5687b3
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-01-02 23:17:56 -0600

    thecluster.lan repo

--- repositories/homelab.ts
+++ repositories/homelab.ts
@@ -1,12 +1,19 @@
-import { integrationIds, PrivateRepo, PublicRepo } from '../components';
+import { integrationIds, PrivateRepo, PublicRepo } from "../components";
 
-export const pfsense = new PrivateRepo('pfsense', {
-	description: 'pfSense configuration',
+export const pfsense = new PrivateRepo("pfsense", {
+	description: "pfSense configuration",
 });
 
-export const theclusterIo = new PublicRepo('thecluster.io', {
-	description: 'Web portal to THECLUSTER',
+export const theclusterIo = new PublicRepo("thecluster.io", {
+	description: "Web portal to THECLUSTER",
 	requiredChecks: [
-		{ context: 'Build and Test', integrationId: integrationIds.github },
+		{ context: "Build and Test", integrationId: integrationIds.github },
+	],
+});
+
+export const theclusterLan = new PublicRepo("thecluster.lan", {
+	description: "THECLUSTER in your area",
+	requiredChecks: [
+		{ context: "Build and Test", integrationId: integrationIds.github },
 	],
 });
