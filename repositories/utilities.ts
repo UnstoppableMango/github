935de6324910474e7c889defea8453482fbbbdea
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-02-14 17:21:45 -0600

    Create the fenced repo

--- repositories/utilities.ts
+++ repositories/utilities.ts
@@ -1,6 +1,15 @@
 import * as gh from '@pulumi/github';
 import { integrationIds, PublicRepo } from '../components';
 
+export const fenced = new PublicRepo('fenced', {
+	description: 'Parse code fences from anywhere',
+	topics: ['go', 'markdown', 'md', 'fence', 'parser', 'tool'],
+	requiredChecks: [
+		{ context: 'Build and Test', integrationId: integrationIds.github },
+		{ context: 'Docker', integrationId: integrationIds.github },
+	],
+});
+
 export const multiDownloaderNxDocker = new gh.Repository('multi-downloader-nx-docker', {
 	name: 'multi-downloader-nx-docker',
 	description: 'Docker image for anidl/multi-downloader-nx',
