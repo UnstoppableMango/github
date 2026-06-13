fe2cf8f282a309a778148672305f00b12d555a9b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-06-13 17:57:23 -0500

    Change the pfSense TF provider check to just build (#375)

--- repositories/terraform.ts
+++ repositories/terraform.ts
@@ -3,6 +3,6 @@ import { integrationIds, PublicRepo } from '../components';
 export const terraformProviderPfSense = new PublicRepo('terraform-provider-pfsense', {
 	description: 'Terraform provider for pfSense using pfrest',
 	requiredChecks: [
-		{ context: 'Build and Test', integrationId: integrationIds.github },
+		{ context: 'build', integrationId: integrationIds.github },
 	],
 });
