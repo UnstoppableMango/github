0944add8a8593538fd8b9c0d977c1d7b11aff6c5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-08-18 22:20:28 -0500

    inoculant required checks (#412)

--- repositories/homelab.ts
+++ repositories/homelab.ts
@@ -27,7 +27,9 @@ export const azure = new PrivateRepo("azure", {
 
 export const inoculant = new PublicRepo("inoculant", {
 	description: "Kubernetes cluster bootstrapping",
-	requiredChecks: [{ context: "build", integrationId: integrationIds.github }],
+	requiredChecks: [
+		{ context: "build (x86_64-linux)", integrationId: integrationIds.github },
+	],
 });
 
 export const theclusterLan = new PublicRepo("thecluster.lan", {
